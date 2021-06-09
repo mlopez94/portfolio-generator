62d5fa2553f534ef78102d669d0a96bd1ac846a8
Matthew <matthew@tests-MacBook-Air.local>
2021-06-09 16:57:39 -0400

    9.2.4

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
+
+const generatePage = (userName, githubName) => {
+    return `
+        Name: ${userName}
+        Github: ${githubName}
+    `;
+}
+console.log(generatePage('Jane', 'janehub'));
 
 
 
@@ -17,18 +25,22 @@ console.log(profileDataArgs);
 
 
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
 
 
-printProfileData(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+  
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+  
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+
+
+// printProfileData(profileDataArgs);
